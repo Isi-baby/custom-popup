c91fabe0d73d3594cd3c7df87bdf08f43bef118f
Rachael Asiwe <rachaelisioma69@gmail.com>
2023-07-12 11:47:56 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,31 @@
+let dialog = document.querySelector('dialog');
+let output = document.querySelector('.output');
+let cancelBtn = document.querySelector('.cancel')
+const customAlert = (message) => {
+    output.textContent = message;
+    dialog.showModal()
+    // alert(message)
+}
+
+const cancelPopup = () => {
+    dialog.close()
+}
+ 
+cancelBtn.onclick = () => {
+    cancelPopup()
+}
+
+const customConfirm = (message) => {
+    output.innerHTML = `
+        <p> ${message} </p>
+        <button onclick="cancelPopup()"> NO </button>
+        <button onclick="confirmOk()"> YES </button>
+    
+    `
+    dialog.showModal()
+}
+
+const confirmOk = () => {
+    output.textContent = `
+    Congratulations you have been redirected to the main page`
+}
